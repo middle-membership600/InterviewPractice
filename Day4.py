8e515408f51632028d4d4172376153bd0a17de11
Marcos Johnson-Noya <mjohnsonnoya@college.harvard.edu>
2023-08-27 16:46:38 -0400

    Day 4 dumbsol edits

--- Day4.py
+++ Day4.py
@@ -1,17 +1,24 @@
 #O(n) Space
-def DumbSol (nums: list[int]) -> int:
-    solset = set()
-    for i in nums:
-        if i <= 1:
-            continue
-        if i in solset:
-            solset.remove(i)
-        solset.add(i + 1)
-        solset.add(i - 1)
-    return min(solset)
+# def DumbSol (nums: list[int]) -> int:
+#     solset = set()
+#     for i in nums:
+#         if i <= 1:
+#             continue
+#         if i in solset:
+#             solset.remove(i)
+#         solset.add(i + 1)
+#         solset.add(i - 1)
+#     return min(solset)
 
 def DumbSol (nums: list[int]) -> int:
-    
+    s = set(nums)
+    i = 1
+    while i in s:
+        i+=1
+    return i
+
+def DumbSol (nums: list[int]) -> int:
+
     return 0
 
         
