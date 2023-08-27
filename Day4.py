f072ca761a4d9faedf2398ea55511d38f1f9ea1f
Marcos Johnson-Noya <mjohnsonnoya@college.harvard.edu>
2023-08-27 16:28:48 -0400

    day 4 :(

---
+++ Day4.py
@@ -0,0 +1,21 @@
+#O(n) Space
+def DumbSol (nums: list[int]) -> int:
+    solset = set()
+    for i in nums:
+        if i <= 1:
+            continue
+        if i in solset:
+            solset.remove(i)
+        solset.add(i + 1)
+        solset.add(i - 1)
+    return min(solset)
+
+def DumbSol (nums: list[int]) -> int:
+    
+    return 0
+
+        
+
+if __name__ == "__main__":
+    # print("Hello World")
+    print(DumbSol([3, 4, -1, 1]))
