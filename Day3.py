54b17fef8d67bbb19ef085e616d3a0d2d1f0115d
Marcos Johnson-Noya <mjohnsonnoya@college.harvard.edu>
2023-08-27 15:52:42 -0400

    Day 3 edits

--- Day3.py
+++ Day3.py
@@ -3,22 +3,33 @@ class Node:
         self.val = val
         self.left = left
         self.right = right
+    def __str__(self):
+            if self.left is None and self.right is None:
+                return self.val
+            if self.left is None:
+                return self.val + "(" + ", " + str(self.right) + ")"
+            if self.right is None:
+                return self.val + "(" + str(self.left) + ", " + ")"
+            return self.val + "(" + str(self.left) + ", " + str(self.right) + ")"
 
 def serialize(root) -> str:
-    if not root.val:
-        return " "
-    strval = str(root.val)
-    return strval + "," + serialize(root.left) + "," + serialize(root.right)
-    serialize(root.left)
-    serialize(root.right)
-def deserialize(root) -> str:
-    '''
-    The recursion for serialize is not correct so it is very difficult for me to come up with the recursion for deserialize.
-    '''
+    if not root:
+        return "#"
+    return str(root.val) + "," + serialize(root.left) + "," + serialize(root.right)
 
-
-
-
+def deserialize(data) -> Node:
+    def helper():
+        val = next(vals)
+        if val == '#':
+            return None
+        node = Node(str(val))
+        node.left = helper()
+        node.right = helper()
+        return node
+    vals = iter(data.split(','))
+    return helper()
+### this approach is O(n) because it iterates through each node of the tree a fixed number of timesc
 if __name__ == "__main__":
     node = Node('root', Node('left', Node('left.left')), Node('right'))
-    print(serialize(node))
+    # print(deserialize(serialize(node)))
+    assert deserialize(serialize(node)).left.left.val == 'left.left'
