19a34f5abd145b63ac22d9ba53b3b15667005145
Marcos Johnson-Noya <mjohnsonnoya@college.harvard.edu>
2023-08-31 16:14:14 -0400

    added memoization

--- Day7.py
+++ Day7.py
@@ -1,18 +1,23 @@
 def SmartSol(msg : str) -> int:
-    msglist = list(msg)
-    total = 0
-    def RecSol(msglist : list[int]) -> int:
-        global total
-        if msglist[0] == '0':
-            return 0
-        elif len(msglist) <= 1: # This covers empty string
+    memo = {}
+    def RecSol(index: int) -> int:
+        if index in memo:
+            return memo[index]
+        if index == len(msg):
             return 1
-        if int(s[:2]) <= 26:
-            total += RecSol(msglist[2:])
-
-        total += RecSol(msglist[1:])
+        if msg[index] == '0':
+            return 0
+        if index == len(msg) - 1:
+            return 1
         
-    return RecSol(msglist)   
+        if (int(msg[index] + msg[index + 1]) <= 26):
+            memo[index] = RecSol(index + 1) + RecSol(index + 2)
+        else:
+            memo[index] = RecSol(index + 1) + RecSol(index + 2)
+        return memo[index]
+
+    
+    return RecSol(0)
 
 # not fully getting recursion
 
